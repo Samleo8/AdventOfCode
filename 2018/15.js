6b92f3d0d6d5eb56154569ba377a78692c6dbd5e
Samleo <ec3team@gmail.com>
2018-12-15 20:04:54 +0800

    Added some visualisation

--- 2018/15.js
+++ 2018/15.js
@@ -1,5 +1,5 @@
-input =
-	["################################",
+input =[
+	"################################",
 	"#########################.G.####",
 	"#########################....###",
 	"##################.G.........###",
@@ -8,7 +8,7 @@ input =
 	"################..............##",
 	"######..########...G...#.#....##",
 	"#####....######.G.GG..G..##.####",
-	"#######.#####G............#.####",
+	"#############G............#.####",
 	"#####.........G..G......#...####",
 	"#####..G......G..........G....##",
 	"######GG......#####........E.###",
@@ -17,24 +17,104 @@ input =
 	"######......#########.....E..###",
 	"#####.......#########........###",
 	"#####....G..#########........###",
-	"######.##.#.#########......#####",
+	"#########.#.#########......#####",
 	"#######......#######.......#####",
 	"#######.......#####....E...#####",
 	"##.G..#.##............##.....###",
 	"#.....#........###..#.#.....####",
 	"#.........E.E...#####.#.#....###",
 	"######......#.....###...#.#.E###",
-	"#####........##...###..####..###",
-	"####...G#.##....E####E.####...##",
-	"####.#########....###E.####....#",
-	"###...#######.....###E.####....#",
+	"#####........##...###..#########",
+	"####...G####....E####E.#########",
+	"#####################E.#########",
+	"#####################E.####....#",
 	"####..#######.##.##########...##",
 	"####..######################.###",
-	"################################"];
+	"################################"
+]; //to improve efficiency, some of the entrances/exits have been blocked off because they lead to dead ends
 
 var map = [];
+var elves = []; //contains object of coordinates
+var goblins = []; //contains object of coordinates
 
-for(i=0;i<input.length;i++){
-	map.push(input[i].split(""));
+function init(){
+	var i,j,k,x,y;
+
+	//Create the Map
+	for(i=0;i<input.length;i++){
+		map.push(input[i].split(""));
+	}
+
+	//Make the map objects of
+	//Also find the goblins and elves in the map. We'll use this later
+	for(y=0;y<map.length;y++){
+		for(x=0;x<map[y].length;x++){
+			switch(map[y][x]){
+				case "G":
+					goblins.push({"x":x,"y":y});
+					map[y][x] = {
+						"type":"goblin",
+						"passed":false
+					}
+					break;
+				case "E":
+					elves.push({"x":x,"y":y});
+					map[y][x] = {
+						"type":"elves",
+						"passed":false
+					}
+					break;
+				case ".":
+					map[x][y] = {
+						"type":"empty",
+						"passed":false
+					}
+					break;
+				case ".":
+					map[x][y] = {
+						"type":"empty"
+					}
+					break;
+			}
+		}
+	}
+
+	outputMap();
 }
-console.log(map);
+
+function outputMap(){
+	var table = document.createElement("table");
+
+	for(y=0;y<map.length;y++){
+		var tr = document.createElement("tr");
+		for(x=0;x<map[y].length;x++){
+			var td = document.createElement("td");
+			td.id = "td_"+x+"_"+y;
+
+			switch(map[y][x].type){
+				case "elves":
+					td.innerHTML = "E";
+					break;
+				case "G": case "g":
+					td.className = "goblin";
+					break;
+				case "#":
+					td.className = "wall";
+					break;
+				case ".":
+					td.className = "empty";
+					break;
+			}
+
+			td.className = map[y][x].type;
+
+			tr.appendChild(td);
+		}
+
+		table.appendChild(tr);
+	}
+
+	document.getElementById("out").appendChild(table);
+}
+
+init();
