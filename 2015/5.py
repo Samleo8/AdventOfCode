0ed37b2563088a959ce590c269debfe6ec773ea5
Samleo <ec3team@gmail.com>
2018-12-27 23:18:43 +0800

    Completed 5b

--- 2015/5.py
+++ 2015/5.py
@@ -32,19 +32,29 @@ print(niceStrings)
 
 niceStrings = 0
 
+'''
+lines = [
+    "zgsnvdmlfuplrubt",
+    "vlhagaovgqjmgvwq",
+    "ffumlmqwfcsyqpss",
+    "zztdcqzqddaazdjp",
+    "eavfzjajkjesnlsb",
+    "zzzzabcdefghadfs"
+]
+'''
+
 for s in lines:
     s = s.strip()
-    print("\n"+s)
+    #print("\n"+s)
 
     between = re.findall(r"(a[a-z]a|b[a-z]b|c[a-z]c|d[a-z]d|e[a-z]e|f[a-z]f|g[a-z]g|h[a-z]h|i[a-z]i|j[a-z]j|k[a-z]k|l[a-z]l|m[a-z]m|n[a-z]n|o[a-z]o|p[a-z]p|q[a-z]q|r[a-z]r|s[a-z]s|t[a-z]t|u[a-z]u|v[a-z]v|w[a-z]w|x[a-z]x|y[a-z]y|z[a-z]z)",s)
 
-    if not len(between):
-        print("BAD: No in betweens")
+    if len(between)==0:
         continue
 
     #######
 
-    _pairs = [s[i:i+2] for i in range(len(s))]
+    _pairs = [s[i:i+2] for i in range(len(s)-1)]
 
     #overlaps need to be removed
     pairs = [ _pairs[0] ]
@@ -52,11 +62,13 @@ for s in lines:
         if _pairs[i]!=_pairs[i-1]:
             pairs.append(_pairs[i])
 
+    #however, it is possible that there are aaaa, which counts as a removed overlap, but is still nice/considered a pair
+    actually_ok = re.findall(r"(aaaa|bbbb|cccc|dddd|eeee|ffff|gggg|hhhh|iiii|jjjj|kkkk|llll|mmmm|nnnn|oooo|pppp|qqqq|rrrr|ssss|tttt|uuuu|vvvv|wwww|xxxx|yyyy|zzzz)",s)
+
     #remove all "repeats". if anything is removed, it implies that there are pairs of pairs
     pairs_unique = list(set(pairs))
 
-    if len(pairs_unique)==len(pairs):
-        print("BAD: No pairs")
+    if len(pairs_unique)==len(pairs) and not len(actually_ok):
         continue
 
     niceStrings+=1
